7e07229cdc426a7005ee45a63a74ae6d91c83e32
Jack Bogart <jackbogart0822@gmail.com>
2024-06-30 15:44:05 -0400

    Add source maps for potential debugging
    Useful for tracing bugs if any ever occur.

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,14 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-
   entry: './src/js/index.js',
-
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
     libraryTarget: 'umd',
     clean: true,
   },
-
+  devtool: 'source-map',
   module: {
     rules: [
       {
